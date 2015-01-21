5cbcea0ba0be10a792397ba3688cbf96c54cfbdc
Giedrius Statkevičius <giedrius@localhost.localdomain>
2015-01-21 15:57:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from distutils.core import setup
 
 setup(name='TwitchNotifier',
       version='0.1',
-      py_modules=['library'],
+      description='A tool that sits in the background and notifies you using libnotify if a channel you follow comes online or goes offline',
+      author='Giedrius Statkevičius',
+      author_email='giedrius.statkevicius@gmail.com',
+      url='https://github.com/GiedriusS/TwitchNotifier',
+      scripts=['twitchnotifier'],
+      py_modules=['library.py'],
       )
