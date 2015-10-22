34b2dfc13027ab2dc2a3297004f2ada4f9b3d4bd
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-10-22 20:23:30 +0300

    library: move notification_title['off'] next to ['on']

--- library.py
+++ library.py
@@ -62,12 +62,12 @@ class Settings(object):
                                              self.user_message['off'])
         self.notification_title['on'] = os.getenv('notification_title',
                                                   self.notification_title['on'])
+        self.notification_title['off'] = os.getenv('notification_title_off',
+                                                   self.notification_title['off'])
         self.notification_cont = os.getenv('notification_content',
                                            self.notification_cont)
         self.list_entry = os.getenv('list_entry', self.list_entry)
         self.log_fmt = os.getenv('log_fmt', self.log_fmt)
-        self.notification_title['off'] = os.getenv('notification_title_off',
-                                                   self.notification_title['off'])
         self.notification_cont_off = os.getenv('notification_content_off',
                                                self.notification_cont_off)
         self.list_entry_off = os.getenv('list_entry_off', self.list_entry_off)
